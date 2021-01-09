20b13fd56b08e5e5636bc196fc1aa402eae0784c
Rafael Marins <rafaelcunha@email.com>
2021-01-09 00:16:28 -0300

    primeira aula

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localhost)\mssqllocaldb;Database=CursoMVC;Integrated Security=True");
+        }
+    }
+}
